249eb46049929b06f458fdc93175a9c1b8f211d0
Ryan Kadri <ryankadri1@gmail.com>
2019-01-22 15:24:17 -0500

    Replacing thumbs with graphs :(

--- src/app/components/sidebar/skill-section.tsx
+++ src/app/components/sidebar/skill-section.tsx
@@ -1,6 +1,4 @@
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { SkillDefinition } from "../../types/types";
-import { FontAwesomeIcon } from "../common/fontawesome-icon";
 import { j } from "../common/j";
 import './skill-section.scss';
 
@@ -8,15 +6,21 @@ export const SkillSection = ({ skills }: Props) => (
     <section class="skills">
         <header><h2>Skills</h2></header>
         <ol>{
-            skills.map(skill => <SkillLine skill={skill} /> )
+            skills.slice()
+                .sort((a,b) => b.confidence - a.confidence)
+                .map(skill => <SkillLine skill={skill} /> )
         }</ol>
     </section>
 )
 
 const SkillLine = ({ skill }: { skill: SkillDefinition }) => {
     const unconfidencePercent = (100 - skill.confidence) / 100;
+    const xPos = 50 + 50 * Math.cos(unconfidencePercent * Math.PI);
+    const yPos = 50 - 50 * Math.sin(unconfidencePercent * Math.PI);
     return <li>
-            <FontAwesomeIcon iconDef={ faThumbsUp } style={ `transform: rotate(-${unconfidencePercent * 180}deg)`} />
+            <svg viewBox="0 0 100 50" class="confidence-chart">
+                <path d={`M 0 50 A 50 50 1 0 1 ${xPos} ${yPos} L 50 50`}></path>
+            </svg>
             { skill.name }
         </li>
 }
