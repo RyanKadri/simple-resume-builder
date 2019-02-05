7504a3a0e517cf2ac895e1f759a79e65c172bf68
Ryan Kadri <ryankadri1@gmail.com>
2019-02-04 19:53:18 -0500

    Removing data

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -2,7 +2,7 @@ import { ResumeData } from "../types/types";
 import { education } from "./education";
 import { experience } from "./experience";
 import { interests } from "./interests";
-import { personal } from "./personal";
+import { personal } from "./personal-info";
 import { projects } from "./projects";
 import { skills } from "./skills";
 
